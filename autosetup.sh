3fd87cda00d3b37d87648103b98d720c7e536af6
Seedy-Zoew <123393161+Seedy-Zoew@users.noreply.github.com>
2023-05-15 13:57:28 -0400

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get upgrade -y; sudo apt-get update -y; sudo apt-get install -y aircrack-ng; sudo apt-get install -y kismet; git clone https://github.com/hobobandy/kestrel && cd kestrel/plugin-kestrel && sudo make install && cd ..; git clone https://github.com/daiceman825/kismet.git; cd kismet; pip3 install -r requirements.txt; cd ..; sudo apt-get install realtek-rtl88xxau-dkms
+sudo apt-get upgrade; sudo apt update; sudo apt install -y aircrack-ng; sudo apt install gpsd; sudo apt install cgps;  sudo apt install kismet; sudo apt install kismet kismet-plugin; git clone https://github.com/hobobandy/kestrel; cd kestrel/plugin-kestrel; sudo make install; cd ../..; git clone https://github.com/daiceman825/kismet.git; cd kismet; pip3 install -r requirements.txt; cd ..; sudo apt-get install realtek-rtl88xxau-dkms
